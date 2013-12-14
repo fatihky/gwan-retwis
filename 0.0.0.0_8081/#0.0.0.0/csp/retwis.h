8b7bd8c08b84e6326b4e90ad73ea71b068f43dac
Fatih Kaya <1994274@gmail.com>
2013-12-14 17:05:24 +0200

    rewits

--- 0.0.0.0_8081/#0.0.0.0/csp/retwis.h
+++ 0.0.0.0_8081/#0.0.0.0/csp/retwis.h
@@ -493,7 +493,10 @@ LINE_
 
 LINE_
 if(pass_sha == NULL) printf("pass_sha is null\n");
-    redisReply *rr2 = redisCommand(rc,
+if(pass_sha->ptr == NULL) printf("pass_sha->ptr is null\n");
+if(username == NULL) printf("username is null\n");
+if(uid == 0) printf("(uid == 0)\n");
+     redisReply *rr2 = redisCommand(rc,
      "HMSET uid:%llu username %s pass %s"
     , uid, username, pass_sha->ptr);
 if (rr2 == NULL) LINE_
